62685014bd1e26a096c85dd38509f2b649177b6b
Peter Mezei <mezeipetister@gmail.com>
2025-05-10 17:12:52 +0200

    ....

--- toto/src/main.rs
+++ toto/src/main.rs
@@ -1,6 +1,8 @@
 use core::convert::TryInto;
 use core::error;
+use std::sync::mpsc::{Receiver, Sender};
 
+use chrono::{Datelike, NaiveDate, NaiveDateTime, NaiveTime, Timelike};
 use ds3231::{
     Config as DsConfig, InterruptControl, Ocillator, SquareWaveFrequency, TimeRepresentation,
     DS3231,
@@ -22,12 +24,33 @@ use esp_idf_svc::wifi::{BlockingWifi, EspWifi};
 use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
 use log::info;
 use serde::Deserialize;
-use std::sync::Arc;
+use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::time::Duration;
 
 const SSID: &'static str = "Mezei";
 const PASSWORD: &'static str = "Hs-fhU%3~MC";
+
+fn bg_demo() {
+    // Create a channel
+    let (tx, rx): (Sender<i32>, Receiver<i32>) = mpsc::channel();
+
+    // Background task to listen to the channel and log messages
+    let listener_handle = thread::spawn(move || {
+        for received in rx {
+            info!("Received: {}", received);
+        }
+    });
+
+    // Background task to count to 10 and send messages via the channel
+    let sender_handle = thread::spawn(move || {
+        for i in 1..=10 {
+            info!("Sending: {}", i);
+            tx.send(i).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+}
 
 fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
@@ -36,6 +59,9 @@ fn main() -> anyhow::Result<()> {
     unsafe {
         esp_idf_sys::esp_tls_init_global_ca_store();
     }
+
+    // Run bg demo
+    bg_demo();
 
     let peripherals = Peripherals::take()?;
     let sys_loop = EspSystemEventLoop::take()?;
@@ -46,11 +72,15 @@ fn main() -> anyhow::Result<()> {
         sys_loop,
     )?;
 
+    use std::sync::mpsc;
+    use std::sync::mpsc::{Receiver, Sender};
     use std::sync::Mutex;
 
     let led = PinDriver::output(peripherals.pins.gpio2)?;
     let led = Arc::new(Mutex::new(led));
     let led_clone = Arc::clone(&led);
+
+    connect_wifi(&mut wifi)?;
 
     thread::spawn(move || loop {
         let mut led = led_clone.lock().unwrap();
@@ -60,15 +90,47 @@ fn main() -> anyhow::Result<()> {
         thread::sleep(Duration::from_millis(500));
     });
 
-    connect_wifi(&mut wifi)?;
-
     #[derive(Deserialize)]
     struct TimeApiResponse {
         datetime: String,
     }
 
-    let current_local_time = read_time_from_ds3231()?;
+    // Initialize device with I2C
+    let i2c = I2cDriver::new(
+        peripherals.i2c0,
+        peripherals.pins.gpio21,
+        peripherals.pins.gpio22,
+        &I2cConfig {
+            ..Default::default()
+        },
+    )?;
+
+    let config = DsConfig {
+        time_representation: TimeRepresentation::TwentyFourHour,
+        square_wave_frequency: SquareWaveFrequency::Hz1,
+        interrupt_control: InterruptControl::SquareWave,
+        battery_backed_square_wave: false,
+        oscillator_enable: Ocillator::Enabled,
+    };
+
+    info!("Initializing DS3231...");
+
+    let mut rtc = DS3231::new(i2c, 0x68);
+
+    info!("DS3231 initialized");
+
+    // Configure the device
+    rtc.configure(&config).expect("Failed to configure DS3231");
+
+    let current_local_time = read_time_from_ds3231(&mut rtc)?;
     info!("Current local time: {current_local_time}");
+
+    let today = NaiveDateTime::new(
+        NaiveDate::from_ymd(2025, 5, 9),
+        NaiveTime::from_hms(20, 56, 0),
+    );
+
+    set_dtime_to_ds3231(&mut rtc, today)?;
 
     let current_time = fetch_current_time()?;
     info!("Current time from API: {current_time}");
@@ -79,7 +141,7 @@ fn main() -> anyhow::Result<()> {
 
     info!("Shutting down in 5s...");
 
-    std::thread::sleep(core::time::Duration::from_secs(5));
+    std::thread::sleep(core::time::Duration::from_secs(20));
 
     Ok(())
 }
@@ -133,37 +195,22 @@ fn connect_wifi(wifi: &mut BlockingWifi<EspWifi<'static>>) -> anyhow::Result<()>
     Ok(())
 }
 
-fn read_time_from_ds3231() -> anyhow::Result<chrono::NaiveDateTime> {
-    // Create configuration
-    let config = DsConfig {
-        time_representation: TimeRepresentation::TwentyFourHour,
-        square_wave_frequency: SquareWaveFrequency::Hz1,
-        interrupt_control: InterruptControl::SquareWave,
-        battery_backed_square_wave: false,
-        oscillator_enable: Ocillator::Enabled,
-    };
-
-    // Initialize peripherals
-    let mut peripherals = Peripherals::take()?;
-
-    // Initialize device with I2C
-    let i2c = I2cDriver::new(
-        peripherals.i2c0,
-        &mut peripherals.pins.gpio21,
-        &mut peripherals.pins.gpio22,
-        &I2cConfig {
-            ..Default::default()
-        },
-    )?;
-    let mut rtc = DS3231::new(i2c, 0x68);
-
-    // Configure the device
-    rtc.configure(&config);
-
+fn read_time_from_ds3231(rtc: &mut DS3231<I2cDriver>) -> anyhow::Result<chrono::NaiveDateTime> {
     // Get current date/time
     let datetime = rtc.datetime().unwrap();
-
     Ok(datetime.into())
+}
+
+fn set_dtime_to_ds3231(
+    rtc: &mut DS3231<I2cDriver>,
+    datetime: chrono::NaiveDateTime,
+) -> anyhow::Result<()> {
+    // Set date/time
+    let bcd_datetime = NaiveDateTime::new(datetime.date(), datetime.time());
+
+    rtc.set_datetime(&bcd_datetime).unwrap();
+
+    Ok(())
 }
 
 fn bcd_to_decimal(bcd: u8) -> u8 {
