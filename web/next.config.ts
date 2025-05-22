dfcf3780b9100610fdf8559bf37a09e3bbd0d360
Peter Mezei <mezeipetister@gmail.com>
2025-05-22 17:20:29 +0200

    ..

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,10 +1,19 @@
 import type {NextConfig} from 'next';
 
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+const nextConfig: NextConfig = {
+  output: 'standalone',
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
   },
+  //   experimental: {
+  //     serverActions: {
+  //       allowedOrigins: ['localhost'],
+  //       // allowedForwardedHosts: ["localhost:3000"],
+  //       // ^ You might have to use this property depending on your exact
+  //       version.
+  //     }
+  //   }
 };
-
 
 export default nextConfig;
